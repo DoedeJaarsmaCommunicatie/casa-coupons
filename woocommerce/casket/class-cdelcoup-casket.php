15c7d6f64595bc23660e9e384d24e0a243fc0e13
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-11-19 08:51:28 +0100

    added more caskets to the casket coupon
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- woocommerce/casket/class-cdelcoup-casket.php
+++ woocommerce/casket/class-cdelcoup-casket.php
@@ -2,7 +2,7 @@
 
 class Cdelcoup_Casket extends Coupon
 {
-    protected static $casket_id = 7089;
+    protected static $casket_id = [ 7089, 7200, 7104, 7198 ];
     protected static $categories_exclusion = [
         516, 650, 655, 767, 734
     ];
@@ -19,23 +19,18 @@ class Cdelcoup_Casket extends Coupon
             return;
         }
 
-        foreach ($this->getCartItems() as $cartItemKey => $cartItem) {
-            if ($this->hasCasketItem($cartItem)) {
-                $this->has_casket = true;
-                break;
-            }
-        }
+        $this->findCasketInCart();
         
         if (!$this->has_casket) {
             return;
         }
         
-        foreach ($this->getCartItems() as $cartItemKey => $cartItem) {
+        foreach ($this->getCartItems() as $cartItem) {
             /** @var WC_Product $product */
             $product = $cartItem['data'];
             $categories = $product->get_category_ids();
             array_map(function ($category) {
-                if (!in_array($category, static::$categories_exclusion)) {
+                if (!in_array($category, static::$categories_exclusion, true)) {
                     $this->has_wine = true;
                 }
             }, $categories);
@@ -50,8 +45,18 @@ class Cdelcoup_Casket extends Coupon
         }
     }
     
-    protected function hasCasketItem($cartItem)
+    protected function hasCasketItem($cartItem): bool
     {
-        return $cartItem['product_id'] === static::$casket_id;
+        return in_array($cartItem[ 'product_id' ], static::$casket_id, true);
+    }
+    
+    protected function findCasketInCart(): void
+    {
+        foreach ($this->getCartItems() as $cartItem) {
+            if ($this->hasCasketItem($cartItem)) {
+                $this->has_casket = true;
+                break;
+            }
+        }
     }
 }
